c097cc66448522c4205d4d237de44ee6b4ff0a73
derekim23 <s900203@gmail.com>
2024-03-23 18:25:19 -0700

    04_geocentric update

--- 04_Geocentric_Models/04_Geocentric_Models.R
+++ 04_Geocentric_Models/04_Geocentric_Models.R
@@ -14,14 +14,48 @@ data(Howell1)
 
 d <- Howell1[Howell1$age>=18,]
 
+
+
 library(rethinking)
+
+H <- runif(10, 130, 170)
+W <- sim_weight(H, b = 0.5, sd = 5)
+
 m3.1 <- quap(
   alist(
     W ~ dnorm(mu, sigma),
     mu <- a + b * H,
     a ~ dnorm(0, 10),
     b ~ dunif(0, 1),
-    sigma ~ dunif(0, 10),
+    sigma ~ dunif(0, 10)),
     data = list(W = W, H = H)
-  )
 )
+
+precis(m3.1)
+
+dat <- list(W = d$weight, H = d$height)
+
+m3.2 <-  quap(
+    alist(
+      W ~ dnorm(mu, sigma),
+      mu ~ b * H + a,
+      a ~ dnorm(0, 10),
+      b ~ dunif(0, 1),
+      sigma ~ dunif(0, 10)
+    ), data = list(W = W, H = H)
+)
+
+
+precis(m3.2)
+
+dnorm(0,10)
+?dunif
+runif(0, 1)
+
+post <- extract.samples(m3.2)
+
+plot(d$height, d$weight, col = 2, lwd = 3, xlab = 'Height (cm)', ylab = 'Weight (kg)')
+
+for (j in 1:20){
+  abline(a = post$a[j], b = post$b[j], lwd = 1)
+}
