05a0de77c66b6d8f35720d580787293d7eddbc1f
Derek (Finance) Kim <s900203@gmail.com>
2024-04-10 21:29:54 -0700

    d

--- 06_Good_&_Bad_Controls/06.01_Good_&_Bad_Controls.R
+++ 06_Good_&_Bad_Controls/06.01_Good_&_Bad_Controls.R
@@ -47,3 +47,103 @@ dens(post2$b_XY, lwd = 3, col = 2, add = T)
 #We know that the true effect b_XY = 0
 #And that gets only captured when we control for Z
 #i.e. stratify by iy.
+
+
+#Consider the following casual diagram
+#         u
+#       /   \
+# X -> Z ->  Y 
+#
+#u should have arrows going into Z and Y
+
+f <- function(n=100, bXZ=1, bZY = 1){
+  u = rnorm(n)
+  X = rnorm(n)
+  Z = rnorm(n, bXZ * X + u)
+  Y = rnorm(n, bZY * Z + u)
+  bX = coef(lm(Y~X))['X']
+  bXZ = coef(lm(Y~X+Z))['X']
+  return (c(bX, bXZ))
+}
+
+sim <- mcreplicate(1e4, f(), mc.cores = 8)
+
+dens(sim[1,], lwd = 3, xlab = "posterior mean", xlim= c(-1.1,2.1), ylim = c(0, 3))
+dens(sim[2,], lwd = 3, col = 2,  add = T)
+
+
+sim2 <- mcreplicate(1e4, f(bZY = 0), mc.cores = 16)
+
+dens(sim2[1,], lwd = 3, xlab = 'posterior mean', xlim = c(-1.1,0.6))
+dens(sim2[2,], lwd = 3, col =2, add = T)
+#Wrong causal effect estimated by adding Z, a significant variable.
+
+n = 100
+bXZ = 1
+bZY = 0
+u = rnorm(n)
+X = rnorm(n)
+Z = rnorm(n, bXZ * X + u)
+Y = rnorm(n, bZY * Z + u)
+mod = lm(Y~X+Z)
+
+summary(mod) #Z is indeed significant, because there's a fork path
+#from Z to Y.
+
+
+
+#x->y->z pipe: You DO NOT want to stratify by z!
+f <- function(n = 100, bXY = 1, bYZ = 1){
+  x = rnorm(n)
+  y = rnorm(n, bXY * x)
+  z = rnorm(n, bYZ * y)
+  bX = coef(lm(y~x))['x']
+  bXZ = coef(lm(y~x + z))['x']
+  return(c(bX, bXZ))
+}
+
+sim <- mcreplicate(1e4, f(), mc.cores = 32)
+
+dens(sim[1,], lwd = 3, xlab = 'posterior mean', xlim = c(0,1.5),
+     ylim = c(0,6))
+dens(sim[2,], lwd = 3, col =2, add = T)
+
+
+#z->x->y  stratifying by z will cause you to lose precision
+f <- function(n = 100, bZX = 1, bXY = 1){
+  z <- rnorm(n)
+  x <- rnorm(n, bZX * z)
+  y <- rnorm(n, bXY * x)
+  bX <- coef(lm(y~x))['x']
+  bXZ <- coef(lm(y~x + z))['x']
+  return(c(bX, bXZ))
+}
+
+sim <- mcreplicate(1e4, f(), mc.cores = 16)
+
+dens(sim[1,], lwd = 3, xlab = 'posterior mean', xlim = c(0.5,1.5),
+     ylim = c(0,6))
+dens(sim[2,], lwd = 3, col =2, add = T)
+
+
+#Bias amplification
+#    u
+#   | \
+#z->x->y
+
+f <- function(n = 100, bZX = 1, bXY = 1){
+  u <- rnorm(n)
+  z <- rnorm(n)
+  x <- rnorm(n, bZX * z + u)
+  y <- rnorm(n, bXY * x + u)
+  bX <- coef(lm(y~x))['x']
+  bXZ <- coef(lm(y~x+z))['x']
+  c(bX, bXZ)
+}
+
+sim <- mcreplicate(1e4, f(bXY = 0), mc.cores = 16)
+dens(sim[1,], lwd = 3, xlab = 'posterior mean', xlim = c(-0.5,1))
+dens(sim[2,], lwd = 3, col = 2, add = T)
+
+#the true effect is 0, but the effect is biased in y ~ x
+#and this is made even worse in y ~ x + z
